dfc1ec9391ce7bc96577c7cf615c549e7a1eeda9
Bogdan Symchych <bsymchych@advancedmd.com>
2022-09-06 14:11:54 +0300

    Add debug macros without new line in the end

--- src/debug/Debug.cpp
+++ src/debug/Debug.cpp
@@ -1,4 +1,10 @@
 
 #include <Debug.h>
 
+#if DEBUG_ON
+#if DEBUG_USE_TELNET
+
 ESPTelnet telnet;
+
+#endif
+#endif
--- src/debug/Debug.h
+++ src/debug/Debug.h
@@ -5,6 +5,33 @@
 #include <WiFi.h>
 #include "ESPTelnet.h"
 
+#if DEBUG_ON
+  #if DEBUG_USE_SERIAL
+    #define DEBUG_SER_MSG_SL(x)    DEBUG_SERIAL.print(x)
+    #define DEBUG_SER_MSG(x)       DEBUG_SERIAL.println(x); DEBUG_SERIAL.print(DEBUG_PREFIX)
+  #else
+    #define DEBUG_SER_MSG_SL(x)
+  #endif
+
+  #if DEBUG_USE_TELNET
+    #define DEBUG_TEL_MSG_SL(x)    DEBUG_TELNET.print(x)
+    #define DEBUG_TEL_MSG(x)       DEBUG_TELNET.println(x); DEBUG_TELNET.print(DEBUG_PREFIX);
+  #else
+    #define DEBUG_TEL_MSG_SL(x)
+  #endif
+
+  #define DEBUG_MSG_NL(x)       DEBUG_SER_MSG(x); DEBUG_TEL_MSG(x)
+  #define DEBUG_MSG(x)          DEBUG_SER_MSG_SL(x); DEBUG_TEL_MSG_SL(x)
+#else
+  #define DEBUG_MSG_NL(x)
+#endif
+
+#if DEBUG_ON
+#if DEBUG_USE_TELNET
+
 extern ESPTelnet telnet;
+
+#endif
+#endif
 
 #endif
