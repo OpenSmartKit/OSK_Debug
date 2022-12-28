a2202161a3ace6cc03958bae1becd11172955139
Bogdan Symchych <bsymchych@advancedmd.com>
2022-12-28 20:24:40 +0200

    Update debug usage

--- src/debug/Debug.h
+++ src/debug/Debug.h
@@ -7,10 +7,15 @@
 #include <WiFi.h>
 #include "ESPTelnet.h"
 
+/*
+  Use DBG(string, arg1[, arg2, ...]) for formatted debug output like printf(string, arg1[, arg2, ...]). Required at least two arguments.
+  Use DBG_S(arg) to debug output single variable string, number or char
+  Use DGB_D2(arg1, arg2) to debug output two variables in one row. Args can be string, number or char
+*/
 
 #if OSK_DEBUG_ON
   #if OSK_DEBUG_USE_SERIAL
-    #define OSK_DEBUG_SER(format, ...)      DEBUG_SERIAL.print(DEBUG_PREFIX); DEBUG_SERIAL.printf(#format, #__VA_ARGS__); DEBUG_SERIAL.println("");
+    #define OSK_DEBUG_SER(format, ...)      DEBUG_SERIAL.print(DEBUG_PREFIX); DEBUG_SERIAL.printf(format, __VA_ARGS__); DEBUG_SERIAL.println("");
     #define OSK_DEBUG_S_SER(str)            DEBUG_SERIAL.print(DEBUG_PREFIX); DEBUG_SERIAL.println(str);
     #define OSK_DEBUG_S2_SER(str, str2)     DEBUG_SERIAL.print(DEBUG_PREFIX); DEBUG_SERIAL.print(str); DEBUG_SERIAL.println(str2);
   #else
@@ -20,7 +25,7 @@
   #endif
 
   #if OSK_DEBUG_USE_TELNET
-    #define OSK_DEBUG_TEL(format, ...)      DEBUG_TELNET.print(DEBUG_PREFIX); DEBUG_TELNET.printf(#format, #__VA_ARGS__); DEBUG_TELNET.println("");
+    #define OSK_DEBUG_TEL(format, ...)      DEBUG_TELNET.print(DEBUG_PREFIX); DEBUG_TELNET.printf(format, __VA_ARGS__); DEBUG_TELNET.println("");
     #define OSK_DEBUG_S_TEL(str)            DEBUG_TELNET.print(DEBUG_PREFIX); DEBUG_TELNET.println(str);
     #define OSK_DEBUG_S2_TEL(str, str2)     DEBUG_TELNET.print(DEBUG_PREFIX); DEBUG_TELNET.print(str); DEBUG_TELNET.println(str2);
   #else
